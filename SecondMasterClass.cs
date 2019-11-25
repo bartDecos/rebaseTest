dbcfa7affca2cb22a14cab9639b4efb87ede7a20
bartDecos <56830313+bartDecos@users.noreply.github.com>
2019-11-25 13:59:08 +0100

    Added SecondMasterClass

---
+++ SecondMasterClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class MasterClass
+    {
+        public void printMasterBranchMessage()
+        {
+            Console.WriteLine("This is a message from the Master branch but it is version 2.");
+        }
+    }
+}
