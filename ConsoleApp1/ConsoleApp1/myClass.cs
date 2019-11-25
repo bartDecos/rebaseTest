e8087a7532f485f91db22ffb0ea555781e5db80d
Bart van der Lans <b.vanderlans@decos.com>
2019-11-25 13:53:26 +0100

    Added Brance2 class

---
+++ ConsoleApp1/ConsoleApp1/myClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class myClass
+    {
+        public void printBranch2Message()
+        {
+            Console.WriteLine("This was writtern in branch 2!");
+        }
+    }
+}
