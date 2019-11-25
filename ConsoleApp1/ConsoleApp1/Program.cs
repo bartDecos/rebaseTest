6bf2be7696a7483e7a7fa48c151f1ca84fe212e9
Bart van der Lans <b.vanderlans@decos.com>
2019-11-25 14:13:01 +0100

    Branch2 commit 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,8 @@ namespace ConsoleApp1
             Console.WriteLine("This was writtern in the master");
             Console.WriteLine("This was also writtern in the master");
             Console.WriteLine("This was writtern in branch 2");
+
+            Console.WriteLine("This was written in branch2 after the first rebase.");
         }
     }
 }
